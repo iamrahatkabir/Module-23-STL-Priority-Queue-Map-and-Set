b31eea937abe1b08101d5ebfbb541f190bbe5708
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-08-06 00:00:12 +0600

    Maximum priority queue

---
+++ 1_Priority_Queue.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    priority_queue<int> pq;
+
+    pq.push(10);
+    pq.push(5);
+    pq.push(30);
+
+    cout << pq.top() << endl;
+
+    pq.push(100);
+
+    
+    pq.pop();
+    pq.pop();
+    
+    cout << pq.top() << endl;
+
+    return 0;
+}
